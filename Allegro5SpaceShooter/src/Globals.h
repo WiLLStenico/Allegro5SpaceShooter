1880670c9e1dcea5c4341711e78cc93688f26602
William H. Stenico <willstenico@gmail.com>
2013-04-19 23:51:28 -0300

    Fix conflict problem

---
+++ Allegro5SpaceShooter/src/Globals.h
@@ -0,0 +1,15 @@
+/*
+ * Globals.h
+ *
+ *  Created on: 18/04/2013
+ *      Author: WiLL
+ */
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+
+static int WIDTH = 0;
+static int HEIGHT = 0;
+
+#endif /* GLOBALS_H_ */
